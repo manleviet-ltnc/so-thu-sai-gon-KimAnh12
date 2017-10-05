b031008457737862a4c6652bbf89bfec28866201
Đoàn Thị Kim Anh <sieuem36@gmail.com>
2017-10-05 08:44:21 +0700

    hihi

--- SoThuXiGon/SoThuXiGon/Form1.cs
+++ SoThuXiGon/SoThuXiGon/Form1.cs
@@ -17,17 +17,17 @@ namespace SoThuXiGon
             InitializeComponent();
         }
 
-        
+
         private void LisBox_MouseDown(object sender, MouseEventArgs e)
         {
             ListBox lb = (ListBox)sender;
 
             int index = lb.IndexFromPoint(e.X, e.Y);
 
-            if(index !=-1 )
+            if (index != -1)
 
-                       lb.DoDragDrop(lb.Items[index].ToString(),
-                                     DragDropEffects.Copy);
+                lb.DoDragDrop(lb.Items[index].ToString(),
+                              DragDropEffects.Copy);
 
         }
         private void ListBox_DragEnter(object sender, DragEventArgs e)
@@ -40,13 +40,13 @@ namespace SoThuXiGon
 
         private void lstDanhSach_DragDrop(object sender, DragEventArgs e)
         {
-            if( e.Data.GetDataPresent(DataFormats.Text))
+            if (e.Data.GetDataPresent(DataFormats.Text))
             {
                 ListBox lb = (ListBox)sender;
                 lb.Items.Add(e.Data.GetData(DataFormats.Text));
             }
         }
-       private void Save(object sender, EventArgs e)
+        private void Save(object sender, EventArgs e)
         {
             // Mở tập tin
             StreamWriter writer = new StreamWriter("danhsachthu.txt");
@@ -72,12 +72,53 @@ namespace SoThuXiGon
 
             using (StreamReader rs = new StreamReader("danhsachthu.txt"))
             {
+                input = null;
+                while ((input = rs.ReadLine()) != null)
+                {
+                    lstDanhSach.Items.Add(input);
+                }
+            }
+        }
+
+        private void mnuClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void mnuLoad_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader("thumoi.txt");
+                if (reader == null) return;
+            string input = null;
+            while((input = reader.ReadLine())!= null)
+            {
+                lstThuMoi.Items.Add(input);
+            }
+            reader.Close();
+            using (StreamReader rs = new StreamReader("danhsachthu.txt"))
+            {
                  input = null;
                 while ((input = rs.ReadLine()) != null)
                 {
                     lstDanhSach.Items.Add(input);
                 }
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = string.Format("Bây giờ là {0}:{1}:{2} ngày {3} tháng {4} năm {5}",
+                                          DateTime.Now.Hour,
+                                          DateTime.Now.Minute,
+                                          DateTime.Now.Second,
+                                          DateTime.Now.Day,
+                                          DateTime.Now.Month,
+                                          DateTime.Now.Year);
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
         }
     }
 }
